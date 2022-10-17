b171d737961a845d4761beba92df214f3d4040c2
Matthias Bourg <bourg.matt@gmail.com>
2022-10-17 03:06:28 +0200

    Small fix

--- sline-sdk.js
+++ sline-sdk.js
@@ -13,7 +13,7 @@ window.console.log = this.console.log || function () {};
  */
 (function (root) {
   root.Sline = root.Sline || {};
-  root.Sline.VERSION = "1.0.3";
+  root.Sline.VERSION = "1.0.4";
 })(this);
 
 /**
@@ -126,11 +126,11 @@ window.console.log = this.console.log || function () {};
    * @param {string} prefix of href text content that will inserted (optionnal)
    */
   Sline.RequestCheckoutURL = async function (id, prefix) {
-    if (Sline.cart.length < 1) return ;
+    if (Sline.cart.length < 1) return;
     var resUrl = await Sline._GenerateCheckoutURL();
     Sline.checkoutURL = Sline.baseCheckoutURL + resUrl.id;
     var resPrices = await Sline._RequestPrices();
-    var prices = []
+    var prices = [];
     for (var duration in resPrices) {
       prices.push(resPrices[duration].otherInstalmentPrice.amount/100);
     }
@@ -138,7 +138,7 @@ window.console.log = this.console.log || function () {};
     var findlink = document.getElementById(id);
     findlink.href = Sline.checkoutURL;
     if (prefix !== undefined) {
-      findlink.textContent = prefix + minPrice + "€ /mois"
+      findlink.textContent = prefix + minPrice + "€ /mois";
     }
   };
 
