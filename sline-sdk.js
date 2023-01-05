081fa5a78be3810a2210da6946953c1fce3b15f8
david ronsain <david.ronsain@sline.io>
2023-01-05 10:40:30 +0100

    update version

--- sline-sdk.js
+++ sline-sdk.js
@@ -13,7 +13,7 @@ window.console.log = this.console.log || function () {};
  */
 (function (root) {
   root.Sline = root.Sline || {};
-  root.Sline.VERSION = "2.1.2";
+  root.Sline.VERSION = "2.1.3";
 })(this);
 
 /**
